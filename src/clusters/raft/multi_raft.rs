805a3773073be22ca962beaf10ad52520fb4635f
Kim Seon Woo <69591622+seonwoo960000@users.noreply.github.com>
2026-04-18 15:04:14 +0900

    nit

--- src/clusters/raft/multi_raft.rs
+++ src/clusters/raft/multi_raft.rs
@@ -308,17 +308,4 @@ mod tests {
         let unique: HashSet<u32> = seqs.iter().cloned().collect();
         assert_eq!(seqs.len(), unique.len());
     }
-
-    #[test]
-    fn translate_timer_seq_maps_local_to_global() {
-        let me = node("n1");
-        let mut store = MultiRaftStore::new(me.clone());
-
-        store.ensure_group(shard(1, vec![me.clone(), node("n2")]));
-        store.flush();
-
-        let seqs = set_seqs(&store.take_all_timer_commands());
-        assert!(!seqs.contains(&0));
-        assert!(!seqs.contains(&1));
-    }
 }
