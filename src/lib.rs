071a553366dea5abe0760313ad1fb695073e8847
Migorithm <saka90030@gmail.com>
2026-07-22 12:45:06 +0400

    move now_ms()

--- src/lib.rs
+++ src/lib.rs
@@ -189,3 +189,10 @@ impl StartUp {
         }
     }
 }
+
+pub(crate) fn now_ms() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap_or_default()
+        .as_millis() as u64
+}
